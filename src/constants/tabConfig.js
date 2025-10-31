01c9c74634118a253ca4e311e389b8dd0b07e284
Thamalpath <thamalpathsathimantha3@gmail.com>
2025-10-31 16:26:54 +0530

    Add tab config constants file

---
+++ src/constants/tabConfig.js
@@ -0,0 +1,56 @@
+export const TAB_CONFIG = [
+  {
+    id: 1,
+    title: "Create Order",
+    icon: "📝",
+    color: "blue",
+  },
+  {
+    id: 2,
+    title: "Assign Branch",
+    icon: "🏢",
+    color: "green",
+  },
+  {
+    id: 3,
+    title: "Approve Order",
+    icon: "✅",
+    color: "green",
+  },
+  {
+    id: 4,
+    title: "Sales Order",
+    icon: "📋",
+    color: "purple",
+  },
+  {
+    id: 5,
+    title: "Quotation",
+    icon: "💰",
+    color: "yellow",
+  },
+  {
+    id: 6,
+    title: "Payment Receipt",
+    icon: "📎",
+    color: "yellow",
+  },
+  {
+    id: 7,
+    title: "Invoice",
+    icon: "🧾",
+    color: "indigo",
+  },
+  {
+    id: 8,
+    title: "Delivery",
+    icon: "🚛",
+    color: "purple",
+  },
+  {
+    id: 9,
+    title: "Complete",
+    icon: "🎯",
+    color: "indigo",
+  },
+];
